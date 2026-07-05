9cbcf2f7c70f32b2d679982d4e7fb4a5d941a803
Sergey Harchenko <sergdevguy@gmail.com>
2026-07-05 03:07:31 +0300

    feat: изменить туррели

--- src/game/entities/Turret.ts
+++ src/game/entities/Turret.ts
@@ -7,6 +7,10 @@ import { Projectile } from './Projectile';
 import { Truck } from './Truck';
 import { AudioManager } from '../systems/AudioManager';
 
+const BARREL_MUZZLE_OFFSET = 42;
+const PROJECTILE_SPAWN_OFFSET = 38;
+const MUZZLE_FLASH_OFFSET = 48;
+
 type TurretStats = {
     damage: number;
     fireDelay: number;
@@ -27,18 +31,52 @@ export class Turret {
         this.statsProvider = statsProvider;
         this.container = scene.add.container(mount.x, mount.y);
 
-        const base = scene.add.circle(0, 0, 16, 0x2d3a21);
-        base.setStrokeStyle(3, 0x80975a);
-
-        const ring = scene.add.circle(0, 0, 9, COLORS.turret);
-        ring.setStrokeStyle(2, 0x233016);
-
-        const barrel = scene.add.rectangle(16, 0, 34, 7, 0xe8d67a);
-        barrel.setOrigin(0, 0.5);
-        barrel.setStrokeStyle(2, 0x6f5d25);
-
-        this.container.add([barrel, base, ring]);
+        this.createVisual();
         this.truck.add(this.container);
+    }
+
+    private createVisual() {
+        const shadow = this.scene.add.ellipse(0, 9, 38, 14, 0x050705, 0.34);
+
+        const barrelBack = this.scene.add.rectangle(14, 0, 30, 9, 0x5d612f);
+        barrelBack.setOrigin(0, 0.5);
+        barrelBack.setStrokeStyle(2, 0x202512, 1);
+
+        const barrel = this.scene.add.rectangle(18, 0, 30, 6, 0xe5d47d);
+        barrel.setOrigin(0, 0.5);
+        barrel.setStrokeStyle(1, 0x78642b, 1);
+
+        const muzzle = this.scene.add.rectangle(BARREL_MUZZLE_OFFSET, 0, 8, 11, 0x28291b);
+        muzzle.setStrokeStyle(1, 0x0c0d08, 1);
+
+        const base = this.scene.add.ellipse(0, 2, 36, 28, 0x2a321d);
+        base.setStrokeStyle(3, 0x11160d, 1);
+
+        const armorPlate = this.scene.add.rectangle(0, -2, 27, 20, 0x4f6030);
+        armorPlate.setStrokeStyle(2, 0x15190f, 1);
+
+        const ring = this.scene.add.circle(0, -2, 11, COLORS.turret);
+        ring.setStrokeStyle(3, 0x253316, 1);
+
+        const core = this.scene.add.circle(0, -2, 5, 0xd4e989);
+        core.setStrokeStyle(1, 0x465b22, 1);
+
+        const bolts = [
+            this.scene.add.circle(-11, 7, 2, 0xa5b36a),
+            this.scene.add.circle(11, 7, 2, 0xa5b36a)
+        ];
+
+        this.container.add([
+            shadow,
+            barrelBack,
+            barrel,
+            muzzle,
+            base,
+            armorPlate,
+            ring,
+            core,
+            ...bolts
+        ]);
     }
 
     update(deltaSeconds: number, enemies: Enemy[]) {
@@ -65,7 +103,7 @@ export class Turret {
 
         return new Projectile(
             this.scene,
-            pointOnCircle(position, 34, angle),
+            pointOnCircle(position, PROJECTILE_SPAWN_OFFSET, angle),
             angle,
             stats.damage,
             'turret'
@@ -102,15 +140,15 @@ export class Turret {
     }
 
     private flash(position: Point, angle: number) {
-        const muzzle = pointOnCircle(position, 52, angle);
-        const flash = this.scene.add.circle(muzzle.x, muzzle.y, 9, 0xfff2a3, 0.9);
+        const muzzle = pointOnCircle(position, MUZZLE_FLASH_OFFSET, angle);
+        const flash = this.scene.add.circle(muzzle.x, muzzle.y, 7, 0xfff2a3, 0.9);
         flash.setDepth(50);
 
         this.scene.tweens.add({
             targets: flash,
             alpha: 0,
-            scaleX: 1.8,
-            scaleY: 1.8,
+            scaleX: 1.7,
+            scaleY: 1.7,
             duration: 90,
             onComplete: () => flash.destroy()
         });
